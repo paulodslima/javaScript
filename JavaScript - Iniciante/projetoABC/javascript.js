a4b495663e384f3e382f381f8be318cb8d462796
Paulo Diego Dos Santos Lima <74713832+paulodslima@users.noreply.github.com>
2023-03-22 22:26:11 -0300

    Update javascript.js

--- JavaScript - Iniciante/projetoABC/javascript.js
+++ JavaScript - Iniciante/projetoABC/javascript.js
@@ -4,8 +4,12 @@ var c = null
 
 console.log(a)
 console.log(b)
+console.log(c)
 document.write("<h1>Projeto A B C</h1>")
-document.write("<h2>Os valores iniciais de A e B são: A = " + a + ", B = " + b + "</h2>")
+document.write("<h3>O projeto A B C consiste em, usando a variavel C, trocar os valores da variavel A e B.</h3>")
+document.write("<h2>Os valores iniciais de A, B e C são: A = " + a + ", B = " + b + ", C = " + c + "</h2>")
+
+document.write("<p>1. C recebe o valor de A(10) <br>2. A recebe o valor de B(20)<br>3. B recebe o valor atual de C(10)</p>")
 
 c = a
 a = b
@@ -13,4 +17,5 @@ b = c
 
 console.log(a)
 console.log(b)
-document.write("<h2>Os valores finais de A e B são: A = " + a + ", B = " + b + "</h2>")
+console.log(c)
+document.write("<h2>Os valores finais de A, B e C são: A = " + a + ", B = " + b + ", C = " + c + "</h2>")
