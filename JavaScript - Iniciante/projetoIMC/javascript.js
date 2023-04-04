d4d1b46c1772bc12e1b0c7c63113d6908610605f
Paulo Diego Dos Santos Lima <74713832+paulodslima@users.noreply.github.com>
2023-04-03 21:24:38 -0300

    projeto imc
    projeto de uma calculadora de índice de massa corporal que no final diz o resultado da classificação do usuário.

---
+++ JavaScript - Iniciante/projetoIMC/javascript.js
@@ -0,0 +1,31 @@
+var nome = prompt("Digite seu nome:")
+var altura = prompt("Digite sua altura (em cm):")
+var peso = prompt("Digite seu peso")
+var imc
+var classificacao
+
+altura = parseFloat(altura)
+peso = parseFloat(peso)
+altura = altura/100
+imc = peso/(altura*altura)
+
+if (imc < 16) {
+    classificacao = "Baixo peso muito grave"
+} else if (imc >= 16 && imc <= 16.99) {
+    classificacao = "Baixo peso grave" 
+} else if (imc >= 17 && imc <= 18.49) {
+    classificacao = " Baixo peso" 
+} else if (imc >= 18.50 && imc <= 24.99) {
+    classificacao = "Peso normal" 
+} else if (imc >= 25 && imc <= 29.99) {
+    classificacao = "Sobrepeso" 
+} else if (imc >= 30 && imc <= 34.99) {
+    classificacao = "Obesidade grau I" 
+} else if (imc >= 35 && imc <= 39.99) {
+    classificacao = "Obesidade grau II" 
+} else {
+    classificacao = "Obesidade grau III"  
+}
+
+
+document.write("<h1>" + nome + " possui índice de massa corporal igual a " + imc.toString() + ", sendo classificado como: " + classificacao + "</h1>")
